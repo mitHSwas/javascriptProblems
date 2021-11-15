09dcc9aa3d802b501cfa7be7c5d78683b5edad39
Mithun Biswas <mithunbiswasbu92@gmail.com>
2021-11-15 15:08:43 +0600

    Math floor ceil round random done

---
+++ random.js
@@ -0,0 +1,10 @@
+var num = 4.49999999;
+
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+for( var i = 0; i<10; i++){
+    var randomNum = Math.random() * 6;
+    var result4 = Math.round(randomNum);
+    console.log(result4)
+}
